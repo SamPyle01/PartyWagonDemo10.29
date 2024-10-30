5891690b05c1e2ac83b49654489d23b681ec044c
SamPyle01 <sampyle01@gmail.com>
2024-10-29 17:11:51 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  let canvas = createCanvas (400,400);
+  canvas.parent("project")
+
+}
+
+function draw() {
+  background(220);
+  rect(200,200,100,100)
+  ellipse(100,100,100)
+
+}
+
+
